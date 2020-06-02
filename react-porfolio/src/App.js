97b6a040e013feaf088971939ac65d95bed400c0
Oleg Gelfand <oleggelfand.1@gmail.com>
2020-06-01 20:33:02 -0400

    portfolio start

---
+++ react-porfolio/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <header>
+    <div className="App">
+      <div className="App-child">
+          Edit <code>src/App.js</code> and save to reload.
+      </div>
+      <div className="App-child1">
+          two <code>src/App.js</code> and save to reload.
+      </div>
+      <div className="App-child2">
+          two <code>src/App.js</code> and save to reload.
+      </div>
+    </div>
+    </header>
+  );
+}
+
+export default App;
