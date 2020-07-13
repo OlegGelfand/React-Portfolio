547503328594fc0a37a503b328a0d97c88a73a51
Oleg Gelfand <oleggelfand.1@gmail.com>
2020-07-13 16:58:02 -0400

    link to projectdescription

--- react-porfolio/src/Main.js
+++ react-porfolio/src/Main.js
@@ -7,19 +7,21 @@ import ProjectDescription from "./ProjectDescription";
 // import { Link } from "react-router";
 import { Switch, Route } from "react-router";
 function Main() {
-    return (
-      <div>
-    <Switch>
-      <Route exact={true} path="/" component={Home} />
-          <Route exact path="/aboutme" component={About} />
-          <Route exact path="/projects" component={Projects} />
-          <Route exact path="/projects/:title" component={ProjectListItem} /> 
-        <Route exact path="/projectdescription"component={ProjectDescription} />
-      </Switch> 
-  
-
-  </div>
-    );
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path="/" component={Home} />
+        <Route exact path="/aboutme" component={About} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/projects/:title" component={ProjectListItem} />
+        <Route
+          exact
+          path="/projectdescription"
+          component={ProjectDescription}
+        />
+      </Switch>
+    </div>
+  );
 }
 
 export default Main;
