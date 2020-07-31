302f70e47a4a61e4aea861d8a1dfc4c8af76563f
Oleg Gelfand <oleggelfand.1@gmail.com>
2020-07-31 18:47:27 -0400

    publishing

--- react-porfolio/src/index.js
+++ react-porfolio/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
-import {Router} from "react-router";
+// import {Router} from "react-router";
 import {BrowserRouter as Router} from 'react-router-dom'
 
 ReactDOM.render(
